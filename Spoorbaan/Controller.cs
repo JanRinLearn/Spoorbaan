14b47003f78850ead5da57e1af917e094a374be3
NewPCJan-PC\New PC Jan <jrapeldoorn@hotmail.com>
2018-04-16 10:58:12 +0200

    Start met de opdracht

---
+++ Spoorbaan/Controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spoorbaan
+{
+    public class Controller
+    {
+        private int overgangTimer;
+        private int storingTimer;
+        private Spoorbaan spoorbaan;
+
+        public Controller()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void overgangTimer_Elapsed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void storingTimer_Elapsed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void UpdateOvergangen()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void UpdateStations()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ZetSpoorwegStatus()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ZetStoring()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Spoorbaan/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spoorbaan
+{
+    enum OvergangSeinStatus
+    {
+        Aan, Uit, Storing
+    }
+    enum StationSeinStatus
+    {
+        Groen, Rood, Uit, Storing
+    }
+    enum Orientatie
+    {
+        Horizontaal, Verticaal
+    }
+    enum LampStatus
+    {
+        Aan, Uit
+    }
+    enum RailRotatie
+    {
+        _0,_90,_180,_270
+    }
+}
